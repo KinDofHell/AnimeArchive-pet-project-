33638840691868e0691dfa973f747fdc0a5ffb2f
KindofHell <wildghost24@gmail.com>
2023-03-23 00:31:32 +0200

    Fetching anime for anime page

--- client/src/pages/anime/Animepage.tsx
+++ client/src/pages/anime/Animepage.tsx
@@ -1,51 +1,81 @@
 import animePageStyles from "./AnimePage.module.scss";
 
+import { SERVER_HOST } from "../../data/Constant";
+
+import { useParams } from "react-router-dom";
+import { useState, useEffect, Key } from "react";
+
+import axios from "../../utils/axios";
+
 import Image from "../../components/ui/Image/Image";
 import AnimeInfo from "../../components/anime/animeInfo/AnimeInfo";
 import Card from "../../components/ui/cards/Card";
 import Button from "../../components/ui/buttons/Button";
 
 const AnimePage = () => {
-  return (
-    <div
-      className={
-        animePageStyles.anime__page + " " + animePageStyles.title__bigScreen
-      }
-    >
-      <div className={animePageStyles.title}>Attack on Titan</div>
-      <div className={animePageStyles.info__block}>
-        <Image borderMode={true} width="70vw" height="24vw" />
-        <AnimeInfo
-          originalTitle="h"
-          seasonsCount={3}
-          seriesCount={100}
-          years="2016, 2022"
-          status="Ongoing"
-          author="Isayama Isayama"
-          views={300}
-        />
+  const [data, setData] = useState<any>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const { id } = useParams<string>();
+
+  useEffect(() => {
+    axios
+      .get(`/anime/${id}`)
+      .then((res) => {
+        setData(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  if (!isLoading)
+    return (
+      <div
+        className={
+          animePageStyles.anime__page + " " + animePageStyles.title__bigScreen
+        }
+      >
+        <div className={animePageStyles.title}>{data.title}</div>
+        <div className={animePageStyles.info__block}>
+          <Image
+            borderMode={true}
+            width="70vw"
+            height="24vw"
+            src={SERVER_HOST + data.imgCover}
+          />
+          <AnimeInfo
+            originalTitle={data.originTitle}
+            seasonsCount={data.seasons}
+            seriesCount={data.series}
+            years={data.years.join(",")}
+            status={data.status}
+            author={data.author.fullname}
+            views={data.viewsCount}
+          />
+        </div>
+        <div className={animePageStyles.categories__block}>
+          <Card
+            title="Categories"
+            flexColumn={true}
+            flexContentColumn={false}
+            contentPadding="0"
+          >
+            {data.categoriesArray.map((obj: any, index: Key) => (
+              <Button
+                key={index}
+                label={obj.title}
+                link={`/anime/`}
+                width={`${100 - data.categoriesArray.length * 30}%`}
+              />
+            ))}
+          </Card>
+        </div>
+        <div className={animePageStyles.additionalImgs__block}></div>
+        <div className={animePageStyles.description__block}></div>
+        <div className={animePageStyles.characters__block}></div>
       </div>
-      <div className={animePageStyles.categories__block}>
-        <Card
-          title="Categories"
-          flexColumn={true}
-          flexContentColumn={false}
-          contentPadding="0"
-        >
-          <Button label="Action" />
-          <Button label="Action" />
-          <Button label="Action" />
-          <Button label="Action" />
-          <Button label="Action" />
-          <Button label="Action" />
-          <Button label="Action" />
-        </Card>
-      </div>
-      <div className={animePageStyles.additionalImgs__block}></div>
-      <div className={animePageStyles.description__block}></div>
-      <div className={animePageStyles.characters__block}></div>
-    </div>
-  );
+    );
 };
 
 export default AnimePage;
