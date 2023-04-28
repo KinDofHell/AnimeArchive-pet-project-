c2829f7007ce9dde3f4bb9661945be586c6250e4
KindofHell <wildghost24@gmail.com>
2023-04-28 17:02:54 +0300

    Fix character validation

--- server/dist/validations/characterValidation.js
+++ server/dist/validations/characterValidation.js
@@ -1,8 +1,11 @@
 import { body } from "express-validator";
 export const characterValidation = [
     body("fullName", "Invalid fullname").isString().isLength({ min: 6, max: 30 }),
-    body("age", "Invalid age").isString().isLength({ max: 5 }),
-    body("sex", "Invalid sex").isString().isLength({ min: 4, max: 6 }),
+    body("age", "Invalid age").optional().isString().isLength({ max: 5 }),
+    body("sex", "Invalid sex")
+        .optional()
+        .isString()
+        .isLength({ min: 4, max: 10 }),
     body("race", "Invalid race").isString().isLength({ min: 3, max: 15 }),
     body("status", "Invalid status").isString().isLength({ min: 5, max: 12 }),
     body("partners", "Invalid partners").optional().isArray(),
